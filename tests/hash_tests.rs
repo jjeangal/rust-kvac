9d5d7f14e359185bff3e16e0c1b4064fc9fe59c6
JEAN GAL <jgatbkk@gmail.com>
2024-11-21 14:23:43 +0800

    Tests for updated hash function generation.

--- tests/hash_tests.rs
+++ tests/hash_tests.rs
@@ -17,6 +17,40 @@ mod tests {
     }
 
     #[test]
+    fn test_consistency() {
+        let input = "consistent";
+        let prime1 = map_string_to_prime(input);
+        let prime2 = map_string_to_prime(input);
+        assert_eq!(
+            prime1, prime2,
+            "The hash function should be consistent for the same input"
+        );
+    }
+
+    #[test]
+    fn test_friendly() {
+        let input = "test";
+        let input2 = "test2";
+        let prime1 = map_string_to_prime(input);
+        let prime2 = map_string_to_prime(input2);
+        assert_ne!(prime1, prime2, "The result should be drastically different");
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let input = "";
+        let prime = map_string_to_prime(input);
+        println!("prime for empty string: {}", prime);
+    }
+
+    #[test]
+    fn test_long_string() {
+        let input = "a".repeat(1000); // A long string of 1000 'a's
+        let prime = map_string_to_prime(&input);
+        println!("prime for long string: {}", prime);
+    }
+
+    #[test]
     fn test_map_string_to_prime() {
         // let limit = BigNumber::from(1000);
         // let exclude = BigNumber::from(101);
