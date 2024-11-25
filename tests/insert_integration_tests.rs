71631437d8c79a17e964a08eab4d525ad82a59d9
JEAN GAL <jgatbkk@gmail.com>
2024-11-24 20:37:55 -0500

    tests for insert

--- tests/insert_integration_tests.rs
+++ tests/insert_integration_tests.rs
@@ -4,24 +4,80 @@ use rust_kvac::params::PUBLIC_PARAMS;
 use unknown_order::BigNumber;
 
 #[test]
-fn test_insert_with_empty_key() {
+fn test_insert() {
     let params = &*PUBLIC_PARAMS;
-    let c = (BigNumber::one(), BigNumber::from(2u32));
-    let k = "".to_string(); // Empty key
-    let v = BigNumber::from(3u32);
+    let c = (params.one.clone(), params.g.clone());
+    let k = "non_empty_key".to_string();
+    let v = BigNumber::from(5u32);
 
-    let (new_c, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
+    let (_, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
 
     assert_eq!(upd, (k.clone(), v.clone()));
     assert_eq!(proof_k, c);
+}
 
-    let z = (params.hash_function)(&k.clone());
-    let c1_z = c.0.clone().modpow(&z, &params.group.modulus);
-    let c2_v = c.1.clone().modpow(&v, &params.group.modulus);
-    let c2_z = c.1.clone().modpow(&z, &params.group.modulus);
+#[test]
+fn test_insert_with_large_value() {
+    let params = &*PUBLIC_PARAMS;
+    let c = (params.one.clone(), params.g.clone());
+    let k = "large_value_key".to_string();
+    let v = BigNumber::from(1_000_000u32);
 
-    // C = (C1^z * C2^v, C2^z)
-    let new_c1 = c1_z.clone().modmul(&c2_v.clone(), &params.group.modulus);
+    let (_, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
 
-    assert_eq!(new_c, (new_c1, c2_z));
+    assert_eq!(upd, (k.clone(), v.clone()));
+    assert_eq!(proof_k, c);
+}
+
+#[test]
+fn test_insert_with_zero_value() {
+    let params = &*PUBLIC_PARAMS;
+    let c = (params.one.clone(), params.g.clone());
+    let k = "zero_value_key".to_string();
+    let v = BigNumber::from(0u32);
+
+    let (_, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
+
+    assert_eq!(upd, (k.clone(), v.clone()));
+    assert_eq!(proof_k, c);
+}
+#[test]
+fn test_insert_with_empty_key() {
+    let params = &*PUBLIC_PARAMS;
+    let c = (params.one.clone(), params.g.clone());
+    let k = "".to_string();
+    let v = BigNumber::from(3u32);
+
+    let (_, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
+
+    assert_eq!(upd, (k.clone(), v.clone()));
+    assert_eq!(proof_k, c);
+}
+
+#[test]
+fn test_insert_with_special_characters_in_key() {
+    let params = &*PUBLIC_PARAMS;
+    let c = (params.one.clone(), params.g.clone());
+    let k = "!@#$%^&*()_+".to_string();
+    let v = BigNumber::from(3u32);
+
+    let (_, proof_k, upd) = insert(c.clone(), (k.clone(), v.clone()));
+
+    assert_eq!(upd, (k.clone(), v.clone()));
+    assert_eq!(proof_k, c);
+}
+
+#[test]
+fn test_insert_with_duplicate_key() {
+    let params = &*PUBLIC_PARAMS;
+    let c = (params.one.clone(), params.g.clone());
+    let k = "duplicate_key".to_string();
+    let v1 = BigNumber::from(3u32);
+    let v2 = BigNumber::from(7u32);
+
+    let _ = insert(c.clone(), (k.clone(), v1.clone()));
+    let (_, proof_k, upd2) = insert(c.clone(), (k.clone(), v2.clone()));
+
+    assert_eq!(upd2, (k.clone(), v2.clone()));
+    assert_eq!(proof_k, c);
 }
