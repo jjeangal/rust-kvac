978ca72863f1ebcb52f994f0b46cda36f08b52e1
JEAN GAL <jgatbkk@gmail.com>
2024-11-24 19:52:53 -0500

    update insert with modmul and removed unecessary type creations

--- src/insert.rs
+++ src/insert.rs
@@ -15,16 +15,14 @@ pub fn insert(
     let z = (params.hash_function)(&k);
 
     // Calculate C1^z mod modulus and C2^v mod modulus
-    let c1_z = c.0.modpow(&z, &params.group.modulus);
-    let c2_v = c.1.modpow(&v, &params.group.modulus);
+    let c1_z = &c.0.modpow(&z, &params.group.modulus);
+    let c2_v = &c.1.modpow(&v, &params.group.modulus);
+    let c2_z = &c.1.modpow(&z, &params.group.modulus);
 
     // C = (C1^z * C2^v, C2^z)
-    let new_c1 = (c1_z.clone() * c2_v.clone()) % &params.group.modulus;
-    let new_c2 = c.1.modpow(&z, &params.group.modulus);
+    let new_c1 = c1_z.clone().modmul(&c2_v, &params.group.modulus);
 
-    let new_c = (new_c1, new_c2);
-    let proof_k = c;
-    let upd = (k, v);
+    let new_c = (new_c1, c2_z.clone());
 
-    (new_c, proof_k, upd)
+    (new_c, c, (k, v))
 }
