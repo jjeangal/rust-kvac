cef5a5d822ac1191c901fa7eba60958027b852a3
JEAN GAL <jgatbkk@gmail.com>
2024-11-24 19:50:50 -0500

    - use modmul in verify function - update verify tests

--- src/verify.rs
+++ src/verify.rs
@@ -6,27 +6,21 @@ pub fn verify(
     (k, v): (String, BigNumber),
     proof_k: (BigNumber, BigNumber),
 ) -> bool {
-    // Verify the proof Logic
-
     let params = &*PUBLIC_PARAMS;
 
     let z = (params.hash_function)(&k);
 
     let k2_z = proof_k.1.modpow(&z, &params.group.modulus);
-
     let k1_z = proof_k.0.modpow(&z, &params.group.modulus);
     let k2_v = proof_k.1.modpow(&v, &params.group.modulus);
 
-    println!("k2_z: {:?}", k2_z);
-    println!("c.1: {:?}", c.1);
-
-    if ((k1_z * k2_v) % &params.group.modulus) != c.0 {
-        println!("k1_z * k2_v != c.0");
+    if k2_z != c.1 {
+        println!("k2_z != c.1");
         return false;
     }
 
-    if k2_z != c.1 {
-        println!("k2_z != c.1");
+    if k1_z.modmul(&k2_v, &params.group.modulus) != c.0 {
+        println!("k1_z * k2_v != c.0");
         return false;
     }
 
--- tests/verify_integration_tests.rs
+++ tests/verify_integration_tests.rs
@@ -1,7 +1,34 @@
 extern crate rust_kvac;
 
-#[test]
-fn test_verify() {
-    // rust_kvac::verify::verify();
-    // Add assertions
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_kvac::insert::insert;
+    use rust_kvac::params::PUBLIC_PARAMS;
+    use rust_kvac::proof_update::proof_update;
+    use rust_kvac::verify::verify;
+    use unknown_order::BigNumber;
+
+    #[test]
+    fn test_verify() {
+        // Access the public parameters
+        let params = &*PUBLIC_PARAMS;
+
+        // Initial values
+        let c = (params.one.clone(), params.g.clone());
+        let k = "test".to_string();
+        let v = BigNumber::from(8);
+
+        // Insert a key-value pair
+        let (new_c, proof_k, upd) = insert(c, (k.clone(), v.clone()));
+
+        // Update the proof
+        let new_proof_k = proof_update(proof_k, upd.clone());
+
+        // Verify the proof
+        let result = verify(new_c, upd, new_proof_k);
+
+        // Assert that the verification is successful
+        assert!(result, "Verification should succeed with generated data");
+    }
 }
