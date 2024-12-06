518bdf4d25c3eb624afbe8d0fe6a983bbc27639e
JEAN GAL <jgatbkk@gmail.com>
2024-12-05 19:42:14 -0500

    Updated the verify checks with detailed comments

--- src/verify.rs
+++ src/verify.rs
@@ -1,36 +1,87 @@
-use crate::params::PUBLIC_PARAMS;
+use crate::params::*;
 use unknown_order::BigNumber;
 
 /// Verifies the proof for a given key-value pair.
 ///
 /// # Arguments
 ///
-/// * `c` - A tuple representing the current commitment.
-/// * `(k, v)` - A tuple containing the key (as a string) and the value.
-/// * `proof_k` - A tuple representing the proof components.
+/// * `commitment` - The current commitment.
+/// * `(key, value)` - A tuple containing the key (as a string) and the value.
+/// * `proof` - The proof components.
 ///
 /// # Returns
 ///
-pub fn verify(
-    c: (BigNumber, BigNumber),
-    (k, v): (String, BigNumber),
-    proof_k: (BigNumber, BigNumber),
-) -> bool {
+/// `true` if the proof is valid, `false` otherwise.
+pub fn verify(commitment: Commitment, (key, value): (String, BigNumber), proof: Proof) -> bool {
+    // Access the public parameters
     let params = &*PUBLIC_PARAMS;
 
-    let z = (params.hash_function)(&k);
+    // Calculate z using the hash function
+    let z = (params.hash_function)(&key);
 
-    let k2_z = proof_k.1.modpow(&z, &params.group.modulus);
-    let k1_z = proof_k.0.modpow(&z, &params.group.modulus);
-    let k2_v = proof_k.1.modpow(&v, &params.group.modulus);
+    // First Check
+    // ----------------------------------------------------------------------------------
 
-    if k2_z != c.1 {
-        println!("k2_z != c.1");
+    // Calculate (Λk,2)^z mod modulus
+    let lambda_k2_z = proof.first().c2().modpow(&z, &params.group.modulus);
+
+    // Verify that (Λk,2)^z = C2
+    if lambda_k2_z != *commitment.c2() {
+        println!("Verification failed: (Λk,2)^z != C2");
         return false;
     }
 
-    if k1_z.modmul(&k2_v, &params.group.modulus) != c.0 {
-        println!("k1_z * k2_v != c.0");
+    // Second Check
+    // ----------------------------------------------------------------------------------
+
+    // Calculate uk + 1
+    let uk_plus_1 = BigNumber::modadd(proof.u_k(), &BigNumber::one(), &params.group.modulus);
+
+    // Calculate z^(uk + 1) mod modulus
+    let z_uk_plus_1 = z.modpow(&uk_plus_1, &params.group.modulus);
+
+    // Calculate (Λk,1)^(z^(uk + 1)) mod modulus
+    let lambda_k1_z_uk_plus_1 = proof
+        .first()
+        .c1()
+        .modpow(&z_uk_plus_1, &params.group.modulus);
+
+    // Calculate (Λk,2)^v mod modulus
+    let lambda_k2_v = proof.first().c2().modpow(&value, &params.group.modulus);
+
+    // Verify that (Λk,1)^(z^(uk + 1)) * (Λk,2)^v = C1
+    if lambda_k1_z_uk_plus_1.modmul(&lambda_k2_v, &params.group.modulus) != *commitment.c1() {
+        println!("Verification failed: (Λk,1)^(z^(uk + 1)) * (Λk,2)^v != C1");
+        return false;
+    }
+
+    // Third Check
+    // ----------------------------------------------------------------------------------
+
+    // Calculate (Λk,3)^(z^(uk + 1)) mod modulus
+    let lambda_k3_z_uk_plus_1 = proof.second().0.modpow(&z_uk_plus_1, &params.group.modulus);
+
+    // Verify that (Λk,3)^(z^(uk + 1)) = C2
+    if lambda_k3_z_uk_plus_1 != *commitment.c2() {
+        println!("Verification failed: (Λk,3)^(z^(uk + 1)) != C2");
+        return false;
+    }
+
+    // Fourth Check
+    // ----------------------------------------------------------------------------------
+
+    // Calculate (Λk,4)^z mod modulus
+    let lambda_k4_z = proof.second().1.modpow(&z, &params.group.modulus);
+
+    // Calculate (Λk,3)^(Λk,5) mod modulus
+    let lambda_k3_lambda_k5 = proof
+        .second()
+        .0
+        .modpow(&proof.second().2, &params.group.modulus);
+
+    // Verify that (Λk,4)^z * (Λk,3)^(Λk,5) = g
+    if lambda_k4_z.modmul(&lambda_k3_lambda_k5, &params.group.modulus) != params.g {
+        println!("Verification failed: (Λk,4)^z * (Λk,3)^(Λk,5) != g");
         return false;
     }
 
