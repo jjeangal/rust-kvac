c6e6117afac4bb38289fc8d20d0a5bacb9b2179d
JEAN GAL <jgatbkk@gmail.com>
2024-12-09 17:16:56 -0500

    update verify integration tests

--- tests/verify_integration_tests.rs
+++ tests/verify_integration_tests.rs
@@ -4,25 +4,25 @@ extern crate rust_kvac;
 mod tests {
     use super::*;
     use rust_kvac::insert::insert;
-    use rust_kvac::params::PUBLIC_PARAMS;
+    use rust_kvac::params::*;
     use rust_kvac::verify::verify;
     use unknown_order::BigNumber;
 
     #[test]
-    fn test_verify() {
+    fn test_verify_insert() {
         // Access the public parameters
         let params = &*PUBLIC_PARAMS;
 
         // Initial values
-        let c = (params.one.clone(), params.g.clone());
-        let k = "test".to_string();
-        let v = BigNumber::from(8);
+        let commitment = Commitment::new(params.one.clone(), params.g.clone());
+        let key = "test".to_string();
+        let value = BigNumber::from(8);
 
         // Insert a key-value pair
-        let (new_c, proof_k, upd) = insert(c, (k.clone(), v.clone()));
+        let (new_c, proof_k, op) = insert(commitment.clone(), (key, value));
 
         // Verify the proof
-        let result = verify(new_c, upd, proof_k);
+        let result = verify(new_c, (op.key, op.value), proof_k);
 
         // Assert that the verification is successful
         assert!(result, "Verification should succeed with generated data");
@@ -30,42 +30,49 @@ mod tests {
 
     #[test]
     fn test_verify_with_invalid_commitment() {
-        let invalid_commitment = (BigNumber::from(0), BigNumber::from(0)); // Invalid commitment
+        let invalid_commitment: Commitment =
+            Commitment::new(BigNumber::from(0), BigNumber::from(0)); // Invalid commitment
         let key = "test_key".to_string();
         let value = BigNumber::from(5);
-        let proof = (BigNumber::from(1), BigNumber::from(1)); // Dummy proof
 
-        let result = verify(invalid_commitment, (key.clone(), value.clone()), proof);
+        let proof = Proof::new(
+            invalid_commitment.clone(),
+            (BigNumber::from(1), BigNumber::from(1), BigNumber::from(1)),
+            BigNumber::from(1),
+        ); // Dummy proof
+
+        let result = verify(invalid_commitment, (key, value), proof);
         assert!(!result, "Verification should fail with invalid commitment");
     }
 
     #[test]
     fn test_verify_with_invalid_proof() {
-        let valid_commitment = (BigNumber::from(1), BigNumber::from(2)); // Example valid commitment
+        let valid_commitment: Commitment = Commitment::new(BigNumber::from(1), BigNumber::from(2)); // Example valid commitment
         let key = "test_key".to_string();
         let value = BigNumber::from(5);
-        let invalid_proof = (BigNumber::from(0), BigNumber::from(0)); // Invalid proof
 
-        let result = verify(
-            valid_commitment,
-            (key.clone(), value.clone()),
-            invalid_proof,
-        );
+        let invalid_proof: Proof = Proof::new(
+            Commitment::new(BigNumber::from(0), BigNumber::from(0)),
+            (BigNumber::from(0), BigNumber::from(0), BigNumber::from(0)),
+            BigNumber::from(0),
+        ); // Invalid proof
+
+        let result = verify(valid_commitment, (key, value), invalid_proof);
         assert!(!result, "Verification should fail with invalid proof");
     }
 
     #[test]
     fn test_verify_with_invalid_key_value_pair() {
-        let valid_commitment = (BigNumber::from(1), BigNumber::from(2)); // Example valid commitment
+        let valid_commitment: Commitment = Commitment::new(BigNumber::from(1), BigNumber::from(2)); // Example valid commitment
         let invalid_key = "".to_string(); // Invalid key (empty)
         let invalid_value = BigNumber::from(-1); // Invalid value (negative)
-        let proof = (BigNumber::from(1), BigNumber::from(1)); // Example valid proof
+        let proof = Proof::new(
+            valid_commitment.clone(),
+            (BigNumber::from(1), BigNumber::from(1), BigNumber::from(1)),
+            BigNumber::from(1),
+        ); // Example valid proof
 
-        let result = verify(
-            valid_commitment,
-            (invalid_key.clone(), invalid_value.clone()),
-            proof,
-        );
+        let result = verify(valid_commitment, (invalid_key, invalid_value), proof);
         assert!(
             !result,
             "Verification should fail with invalid key-value pair"
