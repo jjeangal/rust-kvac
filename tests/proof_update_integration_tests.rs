157144820db686cdf3d33a22b8636807f332058b
JEAN GAL <jgatbkk@gmail.com>
2024-12-09 17:16:42 -0500

    update proof update tests

--- tests/proof_update_integration_tests.rs
+++ tests/proof_update_integration_tests.rs
@@ -4,7 +4,7 @@ extern crate rust_kvac;
 mod tests {
     use super::*;
     use rust_kvac::insert::insert;
-    use rust_kvac::params::PUBLIC_PARAMS;
+    use rust_kvac::params::*;
     use rust_kvac::proof_update::proof_update;
     use unknown_order::BigNumber;
 
@@ -14,18 +14,18 @@ mod tests {
         let params = &*PUBLIC_PARAMS;
 
         // Initial values
-        let c = (params.one.clone(), params.g.clone());
-        let k = "test".to_string();
-        let v = BigNumber::from(8);
+        let commitment = Commitment::new(params.one.clone(), params.g.clone());
+        let key = "test".to_string();
+        let value = BigNumber::from(8);
 
         // Insert a key-value pair
-        let (.., proof_k, upd) = insert(c, (k.clone(), v.clone()));
+        let (_, proof_k, upd) = insert(commitment.clone(), (key.clone(), value.clone()));
 
         // Update the proof
-        let new_proof_k = proof_update(proof_k.clone(), upd.clone());
+        let new_proof_k = proof_update(key.clone(), proof_k.clone(), upd.clone());
 
         // Consistency check: applying the same update should yield the same result
-        let new_proof_k_again = proof_update(proof_k.clone(), upd.clone());
+        let new_proof_k_again = proof_update(key.clone(), proof_k.clone(), upd.clone());
         assert_eq!(
             new_proof_k, new_proof_k_again,
             "Proof update should be consistent"
