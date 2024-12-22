78540813f2b02a703071f84f56a5ee3250c19ef8
JEAN GAL <jgatbkk@gmail.com>
2024-12-22 00:26:56 -0500

    add errors

--- src/error.rs
+++ src/error.rs
@@ -3,3 +3,10 @@ pub enum ProofError {
     InvalidProof(String),
     NonCoprimePair,
 }
+
+#[derive(Debug)]
+pub enum CryptoError {
+    InvalidGroup(String),
+    InvalidJacobiSymbol,
+    InvalidJacobiInput(String),
+}
