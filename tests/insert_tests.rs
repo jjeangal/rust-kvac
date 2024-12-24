4702896b6e53362f76df8f5dbcdf3ef4c03beaf3
JEAN GAL <jgatbkk@gmail.com>
2024-12-24 16:08:41 -0500

    added inserts tests and format to use test data

--- tests/insert_tests.rs
+++ tests/insert_tests.rs
@@ -1,88 +1,96 @@
+mod common;
+
+#[cfg(test)]
 mod tests {
-    use rust_kvac::insert::insert;
-    use rust_kvac::params::Commitment;
-    use rust_kvac::params::KeyValue;
-    use rust_kvac::params::PUBLIC_PARAMS;
-    use unknown_order::BigNumber;
+    use crate::common::test_data::*;
+    use rust_kvac::{
+        insert::insert,
+        params::{Commitment, PUBLIC_PARAMS},
+    };
+
+    // Test setup
+    fn setup_commitment() -> Commitment {
+        let params = &*PUBLIC_PARAMS;
+        Commitment::new(params.one.clone(), params.g.clone())
+    }
 
     #[test]
-    fn test_insert() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
-        let kv: KeyValue = KeyValue::new("non_empty_key".to_string(), BigNumber::from(1));
+    fn test_basic_insert() {
+        let commitment = setup_commitment();
+        let (new_c, proof_k, upd) = insert(&commitment, &KV1);
 
-        let (_, proof_k, upd) = insert(&commitment, &kv);
-
-        assert_eq!(&upd.key(), &kv.key());
-        assert_eq!(&upd.value(), &kv.value());
+        assert_eq!(&upd.key(), &KV1.key());
+        assert_eq!(&upd.value(), &KV1.value());
         assert_eq!(proof_k.lambda_k(), &commitment);
+        assert_ne!(&new_c, &commitment);
     }
 
     #[test]
     fn test_insert_with_large_value() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
-        let kv: KeyValue =
-            KeyValue::new("large_value_key".to_string(), BigNumber::from(1_000_000u32));
+        let commitment = setup_commitment();
+        let (_, proof_k, upd) = insert(&commitment, &LARGE_KV);
 
-        let (_, proof_k, upd) = insert(&commitment, &kv);
-
-        assert_eq!(&upd.key(), &kv.key());
-        assert_eq!(&upd.value(), &kv.value());
+        assert_eq!(&upd.key(), &LARGE_KV.key());
+        assert_eq!(&upd.value(), &LARGE_KV.value());
         assert_eq!(proof_k.lambda_k(), &commitment);
     }
 
     #[test]
     fn test_insert_with_zero_value() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
-        let kv: KeyValue = KeyValue::new("zero_value_key".to_string(), BigNumber::from(0u32));
+        let commitment = setup_commitment();
+        let (_, proof_k, upd) = insert(&commitment, &ZERO_KV);
 
-        let (_, proof_k, upd) = insert(&commitment, &kv);
-
-        assert_eq!(&upd.key(), &kv.key());
-        assert_eq!(&upd.value(), &kv.value());
-        assert_eq!(proof_k.lambda_k(), &commitment);
-    }
-    #[test]
-    fn test_insert_with_empty_key() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
-        let kv: KeyValue = KeyValue::new("".to_string(), BigNumber::from(3u32));
-
-        let (_, proof_k, upd) = insert(&commitment, &kv);
-
-        assert_eq!(&upd.key(), &kv.key());
-        assert_eq!(&upd.value(), &kv.value());
+        assert_eq!(&upd.key(), &ZERO_KV.key());
+        assert_eq!(&upd.value(), &ZERO_KV.value());
         assert_eq!(proof_k.lambda_k(), &commitment);
     }
 
     #[test]
-    fn test_insert_with_special_characters_in_key() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
-        let kv: KeyValue = KeyValue::new("!@#$%^&*()_+".to_string(), BigNumber::from(3u32));
+    fn test_insert_with_special_characters() {
+        let commitment = setup_commitment();
+        let (_, proof_k, upd) = insert(&commitment, &SPECIAL_CHARS_KV);
 
-        let (_, proof_k, upd) = insert(&commitment, &kv);
-
-        assert_eq!(&upd.key(), &kv.key());
-        assert_eq!(&upd.value(), &kv.value());
+        assert_eq!(&upd.key(), &SPECIAL_CHARS_KV.key());
+        assert_eq!(&upd.value(), &SPECIAL_CHARS_KV.value());
         assert_eq!(proof_k.lambda_k(), &commitment);
+    }
+
+    #[test]
+    fn test_sequential_inserts() {
+        let commitment = setup_commitment();
+
+        // First insert
+        let (commitment2, proof1, upd1) = insert(&commitment, &KV1);
+        assert_eq!(&upd1.key(), &KV1.key());
+        assert_eq!(proof1.lambda_k(), &commitment);
+
+        // Second insert
+        let (_, proof2, upd2) = insert(&commitment2, &KV2);
+        assert_eq!(&upd2.key(), &KV2.key());
+        assert_eq!(proof2.lambda_k(), &commitment2);
     }
 
     #[test]
     fn test_insert_with_duplicate_key() {
-        let params = &*PUBLIC_PARAMS;
-        let commitment = Commitment::new(params.one.clone(), params.g.clone());
+        let commitment = setup_commitment();
 
-        let kv1: KeyValue = KeyValue::new("duplicate_key".to_string(), BigNumber::from(3u32));
-        let kv2: KeyValue = KeyValue::new("duplicate_key".to_string(), BigNumber::from(7u32));
+        let (commitment2, _, _) = insert(&commitment, &KV1);
+        let (_, proof_k, upd2) = insert(&commitment2, &DUPLICATE_KV);
 
-        let _ = insert(&commitment, &kv1);
-        let (_, proof_k, upd2) = insert(&commitment, &kv2);
+        assert_eq!(&upd2.key(), &DUPLICATE_KV.key());
+        assert_eq!(&upd2.value(), &DUPLICATE_KV.value());
+        assert_eq!(proof_k.lambda_k(), &commitment2);
+    }
 
-        assert_eq!(&upd2.key(), &kv2.key());
-        assert_eq!(&upd2.value(), &kv2.value());
-        assert_eq!(proof_k.lambda_k(), &commitment);
+    #[test]
+    fn test_insert_commitment_changes() {
+        let commitment = setup_commitment();
+
+        let (new_commitment, _, _) = insert(&commitment, &KV1);
+
+        assert_ne!(
+            &new_commitment, &commitment,
+            "Commitment should change after insert"
+        );
     }
 }
